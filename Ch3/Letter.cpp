ea623c3e518526087bf42bcf47aa82f4f523f350
FakanBalint <fakan.balint@gmail.com>
2021-09-28 20:32:35 +0200

    ch2 és ch3 elkezdve

---
+++ Ch3/Letter.cpp
@@ -0,0 +1,11 @@
+#include "std_lib_facilities.h"
+
+int main(){
+string FN= "";
+cout <<"Enter the name of the person who you are writing this letter to!" << endl;
+cin >> FN;
+cout <<"Szia " << FN << "," << endl;
+cout <<""<< endl;
+
+return 0;
+}
