bcd345e4ad16ffec6439d32e27c05c9c0fdb9ed8
FakanBalint <fakan.balint@gmail.com>
2021-09-30 01:36:38 +0200

    ch3 kész

--- Ch3/Letter.cpp
+++ Ch3/Letter.cpp
@@ -1,11 +1,52 @@
 #include "std_lib_facilities.h"
 
 int main(){
-string FN= "";
-cout <<"Enter the name of the person who you are writing this letter to!" << endl;
+string FN= "";//Firstname
+string FriendN= ""; //Friend's Name
+char FGender = ' ';//Friend's Gender
+int age = 0; //Friend's Age
+cout <<"Enter the name of the person you want to write to" << endl; 
 cin >> FN;
-cout <<"Szia " << FN << "," << endl;
-cout <<""<< endl;
+cout <<"Hello " << FN << "," << endl;
+cout <<"How are you? I am fine. I miss you.\nWe have met soo long ago are you still doing your hobbies?"<< endl;
 
+cout <<"Enter your other friend's name and gender(f for female m for male)" << endl; 
+cin >> FriendN >>FGender;
+
+if(FGender=='m'){
+    cout << "Have you seen " << FriendN << " lately?"<< endl;
+    cout<< "If you see "<< FriendN<<" please ask him to call me."<< endl;
+}
+else if (FGender=='f'){
+    cout << "Have you seen " << FriendN << " lately?"<< endl;
+    cout<< "If you see "<<FriendN<< " please ask her to call me."<< endl;
+}
+else{
+     cout<<"Incorrect gender"<<endl;
+     exit(0);
+}
+
+cout <<"Enter the age of the recipient" << endl;
+cin>>age;
+
+if (age <= 0 || age >= 100) {
+    error("You are kidding!");
+}
+else{
+cout << "I hear you just had your birthday and you are "<< age << " years old." << endl;
+
+if (age < 12) {
+    cout << "Next year you will be " << age + 1 << "." << endl;
+  } 
+else if (age == 17) {
+    cout << "Next year, you will be able to vote." << endl;
+  } 
+else if (age > 70) {
+    cout << "I hope your are enjoying retirement." << endl;
+  }
+}
+  cout << "Yours sincerely,\n\n\nBálint" <<  endl;
+  
 return 0;
 }
+
